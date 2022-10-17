63e30b7e53e22191e3a7751edb8dfd5fca9baa21
zhangjingkun <786434905@qq.com>
2022-10-17 21:02:08 +0800

    create HelloWorld.java

---
+++ src/main/java/com/atguigu/git/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.atguigu.git;/*
+ @Time 2022-10-17
+ @Author : 张靖坤
+ @Software : Java
+*/
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+        System.out.println("HelloWorld");
+        System.out.println("HelloWorld");
+        System.out.println("HelloWorld");
+        System.out.println("HelloWorld");
+    }
+}
